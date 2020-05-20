a1ecbc791ba0cc5ee0a744fc9035a3858018e49a
Elon Cha <eloncha@YILUNs-MacBook-Pro.local>
2020-05-19 20:22:23 -0400

    update

--- python/linear_model.py
+++ python/linear_model.py
@@ -2,13 +2,14 @@
 import pandas as pd
 import numpy as np
 import sklearn
+import os
 
 # Linear Regression on TESL prostate dataset (source code)
 prostate_url = 'http://web.stanford.edu/~hastie/ElemStatLearn/datasets/prostate.data'
 delimiter = '\t'
 data = pd.read_csv(prostate_url, sep = delimiter).drop(['Unnamed: 0', 'train'], axis = 1)
 data.head(5)
-
+data.to_csv(os.getcwd() + 'prostate.csv')
 def loadDataSet(data):
     numFeat = len(data.columns) - 1
     xArr = data.iloc[:,0:numFeat]
@@ -17,16 +18,27 @@ def loadDataSet(data):
 
 dataMat, labelMat = loadDataSet(data)
 
-def OLSRegress(dataMat, labelMat):
+def OLSRegress(dataMat, labelMat, corrThreshold = 0.7):
     xMat = np.mat(dataMat)
     yMat = np.mat(labelMat).T
     xTx = xMat.T * xMat
     xTy = xMat.T * yMat
-    if np.linalg.det(xTx) == 0.0:
+    # singularity check
+    if np.linalg.det(xTx) == 0:
         print ("This matrix is singular, cannot do inverse")
         return
     else:
-        beta = np.linalg.inv(xTx) * xTy # Alternative: beta = nnp.linalg.solve(xTx, xTy)
+        beta = np.linalg.inv(xTx) * xTy # Alternative: beta = np.linalg.solve(xTx, xTy)
+    # correlation check
+    corr = dataMat.corr()
+    for i in np.arange(0, len(dataMat.columns)):
+        for j in np.arange(i+1, len(dataMat.columns)):
+            if corr.iloc[i,j] >= corrThreshold:
+                iname = corr.columns.values[i]
+                jname = corr.columns.values[j]
+                print ("Caution! {0} and {1} have a correlation coefficient equal to {2}.".format(iname, jname, corr.iloc[i,j]) + '\n')
+    print ('Estimated Coefficient: ')
+    print (beta)
     return beta
 
 beta = OLSRegress(dataMat, labelMat)
